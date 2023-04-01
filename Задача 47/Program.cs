d24353f5f4bee97dcd05c19e0bcb2a7d2aacd2ed
Ekaterina_Popova <agekaterina@list.ru>
2023-04-01 19:58:03 +0300

    <task 47>

---
+++ Задача 47/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+double[,] myArr = new double[3, 4];
+
+Random ran = new Random();
+
+for (int m = 0; m < 3; m++)
+{
+    for (int n = 0; n < 4; n++)
+    {
+       myArr[m, n] = Math.Round(ran.NextDouble() * (20 - (-20)) + (-20) ,1);
+       Console.Write("{0}\t", myArr[m, n]);
+    }
+    Console.WriteLine();
+}
