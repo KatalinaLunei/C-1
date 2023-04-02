788ffe587e4359d5f5841203fa2ebab1517f04df
Ekaterina_Popova <agekaterina@list.ru>
2023-04-02 15:16:40 +0300

    <task 50>

--- Задач 50/Program.cs
+++ Задач 50/Program.cs
@@ -1,33 +1,30 @@
 ﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такой позиции нет.
 
-void Main(string[] args)
-        {
-            Console.Write("Введите строку:");
-            int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
-            Console.Write("Введите столбец:");
-            int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
-            int n = 5; // размер массива
-            int m = 7; // размер массива
-            Random random = new Random();
-            int[,] arr = new int[n, m];
-            Console.WriteLine("Исходный массив:");
-            for (int i = 0; i < arr.GetLength(0); i++)
-            {
-                for (int j = 0; j < arr.GetLength(1); j++)
-                {
-                    arr[i, j] = random.Next(10, 99);
-                    Console.Write("{0} ", arr[i, j]);
-                }
-                Console.WriteLine();
-            }
-            if (pos1 < 0 | pos1 > arr.GetLength(0) - 1 | pos2 < 0 | pos2 > arr.GetLength(1) - 1)
-            {
-                Console.WriteLine("Элемент не существует");
-            }
-            else
-            {
-                Console.WriteLine("Значение элемента массива = {0}", arr[pos1, pos2]);
-            }
-            Console.ReadLine();
+Console.WriteLine("Введите размеры массива");
+int m = Convert.ToInt32(Console.ReadLine()!);
+int n = Convert.ToInt32(Console.ReadLine()!);
+int[,] array = new int[m, n];
  
-        }
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = Convert.ToInt32(new Random().Next(0,21));  
+}
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + "\t  ");
+        Console.WriteLine();
+}
+ 
+ Console.WriteLine("Введите координаты");
+ int a = Convert.ToInt32(Console.ReadLine()!);
+ int b = Convert.ToInt32(Console.ReadLine()!);
+ if (a>m && b>n)
+ Console.WriteLine("такого числа нет");
+ else
+ {
+ object c = array.GetValue(a,b);
+ Console.WriteLine(c);
+ }
